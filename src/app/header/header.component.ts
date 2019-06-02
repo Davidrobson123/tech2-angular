f65c83becc74216fe23d7a758d018414888d7e4f
David <davidrobson2106@hotmail.co.uk>
2019-06-02 21:33:43 +0100

    finish off and delete unnecessary things

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
   logo = '../../assets/leighton-logo.png';
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -4,21 +4,32 @@ import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  templateUrl: './products.component.html'
 })
 
 export class ProductsComponent implements OnDestroy, OnInit {
 
   public results: any;
+  public featuredResult: {
+  };
   subscription: Subscription;
 
   constructor(private productsService: ProductsService) {
   }
 
   ngOnInit() {
+
+    let me = this;
     this.subscription = this.productsService.getProducts()
-        .subscribe(data => this.results = data);
+        .subscribe(data => {
+          data['Items'].map(function(item, i) {
+            if(item.productid === '0m8hjmd721') {
+              me.featuredResult = item;
+            } else {
+              me.results = data;
+            }
+          });
+        });
   }
 
 
